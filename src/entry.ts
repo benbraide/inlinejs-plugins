1e7354bd92ec9533e0f810ad502787a61f0711f7
benbraide <benbraide@yahoo.com>
2023-01-19 17:27:51 +0100

    Enabled entry point; Feature update

---
+++ src/entry.ts
@@ -0,0 +1,15 @@
+import { InlineJSAnimation } from '@benbraide/inlinejs-animation';
+import { InlineJSCollections } from '@benbraide/inlinejs-collections';
+import { InlineJSMoment } from '@benbraide/inlinejs-moment';
+import { InlineJSRouter } from '@benbraide/inlinejs-router';
+import { InlineJSScreen } from '@benbraide/inlinejs-screen';
+import { InlineJSAlert } from '@benbraide/inlinejs-alert';
+
+export function InlineJSPlugins(){
+    InlineJSAnimation();
+    InlineJSCollections();
+    InlineJSMoment();
+    InlineJSRouter();
+    InlineJSScreen();
+    InlineJSAlert();
+}
--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,5 @@ export * from '@benbraide/inlinejs-collections'
 export * from '@benbraide/inlinejs-moment'
 export * from '@benbraide/inlinejs-router'
 export * from '@benbraide/inlinejs-screen'
+
+export * from './entry'
